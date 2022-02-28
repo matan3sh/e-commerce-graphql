f65c8543ae5a2861cc550d4c9472dfb71c39ad4e
Matan Shaviro <matan3sh@gmail.com>
2022-02-28 20:58:09 +0200

    Implement filter by average rating

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,10 +1,29 @@
+const { reviews } = require("../db");
+
 exports.Query = {
   products: (parent, { filter }, { products }) => {
     let filteredProducts = products;
 
     if (filter) {
-      if (filter.onSale) {
+      const { onSale, avgRating } = filter;
+      if (onSale) {
         filteredProducts = filteredProducts.filter((product) => product.onSale);
+      }
+
+      /* Calc ta average rating */
+      if ([1, 2, 3, 4, 5].includes(avgRating)) {
+        filteredProducts = filteredProducts.filter((product) => {
+          let sumRating = 0;
+          let numberOfReviews = 0;
+          reviews.forEach((review) => {
+            if (review.productId === product.id) {
+              sumRating += review.rating;
+              numberOfReviews++;
+            }
+          });
+          const avgProductRating = sumRating / numberOfReviews;
+          return avgProductRating >= avgRating;
+        });
       }
     }
 
